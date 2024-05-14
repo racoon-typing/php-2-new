4a2426f1b4e31b5e6041effb3e5ef1a59a96ceb2
Daniil <eo18622@yandex.ru>
2024-05-14 17:57:05 +0300

    класс задача

--- task-force/init/test.php
+++ task-force/init/test.php
@@ -1,57 +1,87 @@
 <?php
 
-class Task {
-    protected const STATUS_NEW = 'new';
-    protected const STATUS_CANCELED = 'canceled';
-    protected const STATUS_PROGRESS = 'progress';
-    protected const STATUS_DONE = 'done';
-    protected const STATUS_FAILED = 'failed';
-    
-    protected const ACTION_CANCELED = 'cancel';
-    protected const ACTION_RESPOND = 'respond';
-    protected const ACTION_DONE = 'done';
-    protected const ACTION_REFUSE = 'refuse';
+class Task
+{
+    const STATUS_NEW = 'new';
+    const STATUS_IN_PROGRESS = 'proceed';
+    const STATUS_CANCEL = 'cancel';
+    const STATUS_COMPLETE = 'complete';
+    const STATUS_EXPIRED = 'expired';
 
-    private $clientId = null;
-    private $customerId = null;
+    const ACTION_RESPONSE = 'act_response';
+    const ACTION_CANCEL = 'act_cancel';
+    const ACTION_DENY = 'act_deny';
+    const ACTION_COMPLETE = 'act_complete';
+  
+    const ROLE_PERFORMER = 'performer';
+    const ROLE_CLIENT = 'customer';
 
-    public function __construct(int $clientId, int $customerId) {
+    private int $performerId;
+    private int $clientId;
+
+    private string $status;
+
+    /**
+     * Task constructor
+     * @param string $status
+     * @param int $clientId
+     * @param ?int $performerId
+     */
+    public function __construct(string $status, int $clientId, ?int $performerId)
+    {
+        $this->setStatus($status);
+
+        $this->performerId = $performerId;
         $this->clientId = $clientId;
-        $this->customerId = $customerId;
     }
 
-    public function getMap() {
-        return array(
-            self::STATUS_NEW => 'новое',
-            self::STATUS_CANCELED => 'отменено',
-            self::STATUS_PROGRESS => 'в работе',
-            self::STATUS_DONE => 'выполнено',
-            self::STATUS_FAILED => 'провалено',
-        );
+    public function getStatusesMap()
+    {
+        return [
+            self::STATUS_NEW => 'Новое',
+            self::STATUS_CANCEL => 'Отменено',
+            self::STATUS_IN_PROGRESS => 'В работе',
+            self::STATUS_COMPLETE => 'Выполнено',
+            self::STATUS_EXPIRED => 'Провалено',
+        ];
+    }
+    
+    public function getActionsMap()
+    {
+        return [
+            self::ACTION_CANCEL => 'Отменить',
+            self::ACTION_RESPONSE => 'Откликнуться',
+            self::ACTION_COMPLETE => 'Выполнено',
+            self::ACTION_DENY => 'Отказаться',
+        ];
     }
 
-    public function getNextAction($action) {
+    public function setStatus($status) {
+
+    }
+
+    public function getNextAction($action)
+    {
         switch ($action) {
             case self::ACTION_CANCELED:
                 return [];
                 break;
-            
+
             case self::ACTION_RESPOND:
                 return [self::STATUS_PROGRESS];
                 break;
-            
+
             case self::ACTION_CANCELED:
                 return [self::STATUS_PROGRESS];
                 break;
-            
+
             default:
                 # code...
                 break;
         }
     }
 
-    public function getAvailableActions() {
-
+    public function getAvailableActions()
+    {
     }
-
 }
